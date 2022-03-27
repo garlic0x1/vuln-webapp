e69b91ba1ce0f84a6ee88b68f926f3182fb30ce7
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2022-03-27 01:40:50 -0700

    Add files via upload

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE users
 	id INTEGER AUTO_INCREMENT,
 	username varchar(256),
 	password varchar(256),
-	status varchar(64) NULL DEFAULT  'NONE',
+	status varchar(64) NULL DEFAULT 'offline',
 	primary key(id)
 );
