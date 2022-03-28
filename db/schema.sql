fbcea6952ea63607fe15dcded74dfcfb9502dd12
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2022-03-28 03:09:59 -0700

    Add files via upload

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,12 @@ CREATE TABLE users
 	status varchar(256) NULL DEFAULT 'offline',
 	primary key(id)
 );
+
+CREATE TABLE messages
+(
+	id INTEGER AUTO_INCREMENT,
+	sender INTEGER,
+	reciever INTEGER,
+	message varchar(256) NULL DEFAULT 'ping',
+	primary key(id)
+);
